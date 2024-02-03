9f6a1a5140e814e5bf4cf37f4476cf58bb57bfaf
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-02-03 15:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const extendHex = (shortHex) => {
   // write your code here
+	
+	let str = ""
+	let flag = false;
+	
+	for(let i = 0;i<shortHex.length;i++){
+		if(shortHex[i]!=='#'){
+			if(shortHex[i].toUpperCase()=== shortHex[i])
+				flag = true;
+			str+=shortHex[i]
+			str+=shortHex[i]
+		}
+	}
+	if(flag) str = str.toUpperCase();
+	return "#"+str;
+	
+
+
 };
 
 // Do not change the code below.
